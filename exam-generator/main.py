ad588e88957b512f466dbe8463cc288e1cce47df
duyhung-0203 <doduyhung12342k4@gmail.com>
2025-03-30 00:28:55 +0700

    Add files via upload

---
+++ exam-generator/main.py
@@ -0,0 +1,28 @@
+import openai
+import streamlit as st
+
+from app.app import get_app
+
+OPENAI_TOKEN = "token"
+
+def initial_config():
+    """
+    Initial configuration of OpenAI API and streamlit
+    """
+    openai.api_key = st.secrets["OPENAI_TOKEN"]
+
+    st.set_page_config(
+        page_title="Exam generator",
+        page_icon=":pencil2:",
+    )
+
+
+def main():
+    initial_config()
+
+    app = get_app()
+    app.render()
+
+
+if __name__ == '__main__':
+    main()
